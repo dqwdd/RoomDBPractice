0c6515a9eb6ae767f03506eb75305667845075d1
dqwdd <dqwdd@naver.com>
2021-10-20 18:33:04 +0900

    1. AddFragment.kt adjust

--- app/src/main/java/com/emaintec/roomdbpractice/fragments/add/AddFragment.kt
+++ app/src/main/java/com/emaintec/roomdbpractice/fragments/add/AddFragment.kt
@@ -1,18 +1,25 @@
 package com.emaintec.roomdbpractice.fragments.add
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.emaintec.roomdbpractice.R
-import com.emaintec.roomdbpractice.databinding.FragmentListBinding
+import com.emaintec.roomdbpractice.ViewModel.UserViewModel
+import com.emaintec.roomdbpractice.data.UserProfile
+import com.emaintec.roomdbpractice.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
 
-    lateinit var binding : FragmentListBinding
+    private lateinit var mUserViewModel : UserViewModel
+
+    lateinit var binding : FragmentAddBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +31,37 @@ class AddFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_add, container, false)
 
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+
+        binding.buttonAdd.setOnClickListener {
+            insertDataToDatabase()
+        }
+
         return binding.root
     }
+
+    private fun insertDataToDatabase() {
+        val name = binding.edtName.text.toString()
+        val age = binding.edtAge.text.toString()
+
+        if (inputCheck(name, age)){
+            //Create User Object
+            val user = UserProfile(0, name, Integer.parseInt(age.toString()))
+            //Add Data to Database
+            mUserViewModel.addUser(user)
+            Toast.makeText(requireContext(), "Successfully added!", Toast.LENGTH_SHORT).show()
+            //Navigate Back
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+        else {
+            Toast.makeText(requireContext(), "Please fill out all fields.", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun inputCheck(name: String, age: String): Boolean {
+        return !(TextUtils.isEmpty(name) && age.isEmpty())
+    }
+
 
 }
